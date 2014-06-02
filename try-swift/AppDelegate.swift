6d7309f173a1736f5cc880522bff12f6b071cd5b
Mohnish Thallavajhula <i@mohni.sh>
2014-06-02 14:53:21 -0700

    Initial Commit

---
+++ try-swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  try-swift
+//
+//  Created by Mohnish Thallavajhula on 6/2/14.
+//  Copyright (c) 2014 Mohnish Thallavajhula. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+  @IBOutlet var window: NSWindow
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification?) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification?) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ try-swift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  try-swift
+//
+//  Created by Mohnish Thallavajhula on 6/2/14.
+//  Copyright (c) 2014 Mohnish Thallavajhula. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
